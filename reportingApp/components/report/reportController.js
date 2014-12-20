7d90d981f165cfdfe5155892a7123d85bb47c390
haakon-soegnen <hknsgn@gmail.com>
2014-12-20 10:12:12 +0100

    Flytta script i ny struktur

---
+++ reportingApp/components/report/reportController.js
@@ -0,0 +1,78 @@
+(function(){
+
+/**
+*
+* @author: Tore Buer, s180346
+* @author: Eivind Jacobsen, s173466
+* @author: Morten Kristoffersen, s169440
+*
+* @since may.26.2014
+*
+*/
+angular.module('anomalyApp')
+        .controller('ReportController',
+                function($scope, $location, DataService, RestService) {
+                    $scope.anomalyType = DataService.getAnomalyType();
+                    $scope.anomaly = {};
+                    $scope.anomaly.involvedPersons = DataService.getPersonsInvolvedInAnomaly();
+                    $scope.service = DataService;
+                    $scope.pickDate = DataService.getDateTime();
+                    $scope.incidents = {};     
+
+                    getAnomalyTypes();
+
+                    $scope.$watch(function() {
+                        return $scope.anomalyType;
+                    }, function() {
+                        for (var i = 0; i < $scope.anomalyTypes.length; i++) {
+                            if ($scope.anomalyTypes[i].id == $scope.anomalyType.id) {
+                                DataService.setAnomalyType($scope.anomalyTypes[i]);
+                            }
+                        };
+                    }, true);
+                    
+                    $scope.setDate = function(){
+                        var day = $scope.pickDate.substring(0,2);
+                        var month = $scope.pickDate.substring(3,5)-1;
+                        var year = $scope.pickDate.substring(6,10) ;
+                        
+                        var hour = $scope.pickDate.substring(11,13);
+                        var minute = $scope.pickDate.substring(14,16);
+                        
+                        $scope.theDate = new Date(year, month, day, hour, minute);
+                        DataService.setDateTime($scope.theDate);
+                    };
+
+                    $scope.$watch('service.readAnomaly()', function(anomaly){
+                       $scope.anomaly = anomaly; 
+                    });
+                    
+                    
+                    $scope.updateAnomaly = function(){
+                        RestService.updateAnomaly($scope.anomaly);
+                      $location.path('/report/3');
+                    };
+                    $scope.createNewReport = function(anomaly) {
+                        RestService.saveAnomaly(anomaly);
+                        $location.path('/report/2');
+                    };
+                    $scope.moveIncidentDown = function(id){
+                      DataService.moveIncidentDown(id);  
+                    };
+                    $scope.moveIncidentUp = function(id){
+                      DataService.moveIncidentUp(id);  
+                    };
+                    
+                    function getAnomalyTypes() {
+                    $scope.anomalyTypes = [];
+                    RestService.getAnomalyTypes()
+                            .success(function(anomalyTypes) {
+                                $scope.anomalyTypes= anomalyTypes;
+                            })
+                            .error(function(error) {
+                                //Handle error
+                            });
+                };           
+            });
+
+})();
