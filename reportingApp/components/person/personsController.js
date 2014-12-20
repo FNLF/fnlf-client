5ed2be6aec2c177787ae24bbbedd43724bcde867
haakon-soegnen <hknsgn@gmail.com>
2014-12-20 21:20:02 +0100

    Opprydding

--- reportingApp/components/person/personsController.js
+++ reportingApp/components/person/personsController.js
@@ -16,27 +16,6 @@ angular.module('reportingApp')
                 $scope.service = DataService;
                 $scope.choosePersons = "Henter fra databasen...";
                 $scope.anomaly = DataService.readAnomaly();
-                getInvolved();
-                
-                function getInvolved(){
-                    $scope.personsInvolved = [];
-                    angular.forEach($scope.anomaly.involvedPersons, function(involved){
-                       $scope.personsInvolved.push(involved.id);
-                    });
-                };
-
-                $scope.$watch(function() {
-                    return $scope.personsInvolved;
-                }, function() {  
-
-                   var involved = [];
-                    angular.forEach($scope.personsInvolved, function(selected) {
-                        involved.push(getPersonById(selected));
-                    });
-                    $scope.anomaly.involvedPersons = involved;
-                }, true);
-                
-
 
                 $scope.$watch('service.readAnomaly()', function(newValue) {
                     $scope.anomaly = newValue;                    
@@ -71,29 +50,6 @@ angular.module('reportingApp')
 					}
 
 				};
-
-                function getPersons() {
-                    $scope.personsFromDb = [];
-                    $scope.personStatus = "Henter fra databasen";
-                    $scope.showLoader = true;
-                    RestService.getPersons()
-                            .success(function(persons) {
-                                $scope.personsFromDb= persons._items;
-                                $scope.choosePersons = "Velg involvert(e) person(er)";
-                                $scope.showLoader = false;
-                            })
-                            .error(function(error) {
-                                $scope.personStatus = 'Fikk ikke hentet personer fra databasen: ' + error;
-                            });
-                };
-
-                function getPersonById(id) {
-                    for (var i = 0; i < $scope.personsFromDb.length; i++) {
-                        if ($scope.personsFromDb[i].id === id) {
-                            return $scope.personsFromDb[i];
-                        }
-                    }
-                };
 
                 $scope.saveNewPerson = function() {
                     RestService.savePerson($scope.newPerson);
