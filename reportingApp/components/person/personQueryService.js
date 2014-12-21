4b91f84e148481b9b503af8345eefec03bd84003
haakon-soegnen <hknsgn@gmail.com>
2014-12-21 23:37:35 +0100

    Using the right search url

--- reportingApp/components/person/personQueryService.js
+++ reportingApp/components/person/personQueryService.js
@@ -4,7 +4,7 @@ angular.module('reportingApp')
 	.service('PersonQueryService', ['$http', function($http) {
         var urlBase = '/api/v1';
         this.getPersonsByName = function(name) {
-        	return $http.get(urlBase + '/melwin/users?q='+name)
+        	return $http.get(urlBase + '/melwin/users/search?q='+name)
 			};
 
      }]);
