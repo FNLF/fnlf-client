b35c151ee830981f4df9c5971fff07068c7804f6
Einar Huseby <einar.huseby@gmail.com>
2015-02-05 13:24:12 +0100

    Added module resolve to se if we can easily resolve fropm id's

--- app/reporting/app.js
+++ app/reporting/app.js
@@ -10,24 +10,25 @@
 (function () {
 
 	var reportingApp = angular.module('reportingApp', 
-			['ngRoute', 'ui.select', 'ngSanitize', 'ngCookies',
-			 'ui.bootstrap',
+			['ngRoute', 'ngSanitize', 'ngCookies',
+			 'ui.bootstrap', 'ui.select',
 			 //'ui.bootstrap.alert','ui.bootstrap.collapse','ui.bootstrap.datepicker', 'ui.bootstrap.typeahead',
-			 'angular-loading-bar', 'fnlf-login', 
-			 'mgcrea.ngStrap.modal', 'mgcrea.ngStrap.aside'/*,'ngMockE2E'*/]);
+			 'angular-loading-bar', 
+			 'mgcrea.ngStrap.modal', 'mgcrea.ngStrap.aside',
+			 'fnlf-login', 'resolve',
+			 /*,'ngMockE2E'*/
+			 ]);
 
 	reportingApp.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
 		cfpLoadingBarProvider.includeBar = true;
 		cfpLoadingBarProvider.includeSpinner = true;
 	}]);
-
-
+	
 	reportingApp.run(function ($rootScope, $location, $cookieStore) {
 
 		$rootScope.$on('$viewContentLoaded', function () {
 			delete $rootScope.error;
 		});
-
 
 		$rootScope.initialized = true;
 	});
