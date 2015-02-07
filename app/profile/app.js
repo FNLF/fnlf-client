4575342fb856061ec6a59b4947cbb97ec7077666
Einar Huseby <einar.huseby@gmail.com>
2015-02-07 11:34:34 +0100

    Profile app coming together with angular-file-upload

--- app/profile/app.js
+++ app/profile/app.js
@@ -1,6 +1,7 @@
 (function() {
 
 	var profileApp = angular.module('profileApp', [ 'ngRoute', 'ui.bootstrap',
+	                                                'angularFileUpload',
 	                                                'ui.select', 'ngSanitize', 'ngCookies', 'angular-loading-bar','fnlf-login','xeditable' ]);
 
 	profileApp.config([ 'cfpLoadingBarProvider',
@@ -25,9 +26,6 @@ angular.module("profileApp")
 			   
 		   });
 			  
-		   
-		   
-		   
 //		   var _etag = u._etag;
 //		   var _id = u._id;
 //		   
@@ -35,8 +33,87 @@ angular.module("profileApp")
 //		   delete(u._id);
 		   //...
 		   
-		   
 	   });
+
+/**
+ * Avatar
+ * 
+ * Upload
+ */
+angular.module("profileApp").controller("avatarController", 
+		['$scope', '$upload', 'userService', '$timeout','$rootScope', 
+		 function($scope, $upload, userService, $timeout, $rootScope) {
+	
+	var urlBase = '/api/v1';
+	
+	$scope.usingFlash = FileAPI && FileAPI.upload != null;
+	$scope.fileReaderSupported = window.FileReader != null && (window.FileAPI == null || FileAPI.html5 != false);
+	
+	//Always get the avatar!
+	
+	$scope.getAvatar = function(file) {
+		
+		userService.getUserAvatar().then(function(data) {
+			file.dataurl = 'data:'+data.avatar.content_type+';base64,'+data.avatar.file;
+	});	   
+//		   var fileReader = new FileReader();
+//			fileReader.readAsDataURL(file);
+//			fileReader.onload = function(e) {
+//				$timeout(function() {
+//					file.dataUrl = e.target.result;
+//				});
+//			}
+//		   
+//		   picFile[0].dataUrl
+		   
+	   };
+	
+	$scope.$watch('files', function () {
+        $scope.upload($scope.files);
+    });
+    
+	$scope.upload = function (files) {
+		console.log("UPLOAAAAAAAAAD");
+		
+        if (files && files.length) {
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                $upload.upload({
+                    url: urlBase + '/users/' + $rootScope.userid,
+                    //fields: {'username': $scope.username},
+                    file: file
+                }).progress(function (evt) {
+                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                    console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+                }).success(function (data, status, headers, config) {
+                    console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+                });
+            }
+        }
+    };
+    
+	$scope.generateThumb = function(file) {
+		
+		if (file != null) {
+			console.log("Filereader");
+			console.log($scope.fileReaderSupported);
+			if ($scope.fileReaderSupported && file.type.indexOf('image') > -1) {
+				console.log("THUMBING");
+					$timeout(function() {
+					var fileReader = new FileReader();
+					fileReader.readAsDataURL(file);
+					fileReader.onload = function(e) {
+						$timeout(function() {
+							file.dataUrl = e.target.result;
+						});
+					}
+					});
+			}
+		}
+	}
+	
+	
+}]);
 
 angular.module("profileApp")
 	   .service("userService",['$http','$q','$rootScope', function($http, $q, $rootScope) {
@@ -57,6 +134,16 @@ angular.module("profileApp")
 				});
 				return (request.then(handleSuccess, handleError));
 	
+			};
+			
+			this.getUserAvatar = function() {
+				
+				var request = $http({
+					method : "get",
+					url : urlBase + '/users/' + $rootScope.username +'?projection={\"avatar\": 1}',
+				});
+				return (request.then(handleSuccess, handleError));
+				
 			};
 	
 			// updating person - no $scope, no injection allowable???
