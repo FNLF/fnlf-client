9ca98ff9cb0918351aeb54548c944bd424924886
Einar Huseby <einar.huseby@gmail.com>
2015-02-21 17:40:48 +0100

    Added summary/datamodel to aside instead

--- app/reporting/components/observation/directives/summary.js
+++ app/reporting/components/observation/directives/summary.js
@@ -1,8 +1,11 @@
-angular.module('reportingApp').directive('summary', function (ObservationService, RestService) {
+angular.module('reportingApp').directive('summary', function (ObservationService, RestService, $aside) {
 	var directive = {};
 
 	directive.restrict = 'E';
-	directive.templateUrl = "components/observation/directives/summary.html";
+	directive.template = function(tElement, tAttrs) { 
+		
+		return '<button type="button" class="btn btn-default pull-right" ng-click="openSummaryAside()"><i class="fa fa-eye fa-fw"></i>Se datamodell</button>';
+	};
 
 	directive.scope = {
 		observation: '='
@@ -12,7 +15,19 @@ angular.module('reportingApp').directive('summary', function (ObservationService
 	directive.link = function ($scope, element, attrs) {
 
 
-
+		$scope.openSummaryAside = function() {
+		    $scope.myAside = $aside({
+		        scope: $scope,
+		        title: 'Datamodellen',
+		        //content: 'My Content', //Static custom content
+		        show: true,
+		        contentTemplate: '/app/reporting/components/observation/directives/summary.html',
+		        template: '/shared/partials/aside.html',
+		        placement: 'full-left',
+		        container: 'body',
+		        animation: 'am-slide-left'
+		        });   
+		};
 	};
 
 	return directive;
