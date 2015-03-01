ae9025d02155b5c6f39a5e70c64fc8104392df7a
Einar Huseby <einar.huseby@gmail.com>
2015-03-01 19:30:13 +0100

    Correct path for resolveuser link

--- shared/resolve/user.js
+++ shared/resolve/user.js
@@ -4,20 +4,16 @@ angular.module('resolve')
 			var directive = {};
 			
 			directive.restrict = 'E';
-			directive.template = '<a href="/app/profile/{{userid}}">{{firstname}} {{lastname}}</a>';
+			directive.template = '<a href="/app/profile/#!/{{userid}}">{{firstname}} {{lastname}}</a>';
 			
 			directive.scope = {
-				observation : '=observation',
 				userid : '='
 			};
 			
 			directive.link = function($scope, element, attrs) {
-				console.log("Resolve USER");
-			
+				
 				ResolveService.getUser($scope.userid).then(
 					function(user) {
-
-						console.log(user);
 						$scope.firstname = user.firstname;
 						$scope.lastname = user.lastname;
 					});
