b3585fb7467aa1d849cbc0acd59c112181128333
Einar Huseby <einar.huseby@gmail.com>
2015-03-09 17:18:32 +0100

    Added workflow state and resolvers

--- app/reporting/components/observation/services/Definitions.js
+++ app/reporting/components/observation/services/Definitions.js
@@ -14,6 +14,18 @@
 				this.id = id;
 				this.name = name;
 			};
+			
+			this.getObservationWorkflowStates = function() {
+				
+				var ows = {'draft': 'Draft', 'withdrawn': 'Trekt tilbake', 'closed': 'Lukket', 'pending_review_hi': 'Avventer HI', 'pending_review_fs': 'Avventer Fagsjef', 'pending_review_su': 'Avventer SU'};
+				
+				return ows;
+			};
+			
+			this.resolveObservationWorkflowState = function(state) {
+				
+				return this.getObservationWorkflowStates()[state];
+			};
 
 
 			this.getObservationTypes = function () {
