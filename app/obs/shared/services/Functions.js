8dad7786d43525d6df41b6cd47a10628edc0786c
Hakon Sognen <hakon.sognen@bouvet.no>
2015-03-18 12:32:53 +0100

    #189 Location dragable in locationsummary

--- app/obs/shared/services/Functions.js
+++ app/obs/shared/services/Functions.js
@@ -2,6 +2,13 @@
 	angular.module('reportingApp')
 		.service('Functions',  function () {
 
+			this.mapDistance = function(lat1,lon1,lat2,lon2){
+
+				var phi1 = lat1.toRadians(), phi2 = lat2.toRadians(), deltaLambda = (lon2-lon1).toRadians(), R = 6371000; // gives d in metres
+				var d = Math.acos( Math.sin(ph11)*Math.sin(ph12) + Math.cos(ph11)*Math.cos(ph12) * Math.cos(deltaLambda) ) * R;
+				return d;
+			};
+
 			this.copy  = function(firstObject,secondObject){
 				for(var k in firstObject){
 					secondObject[k]=firstObject[k];
