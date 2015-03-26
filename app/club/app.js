d49ee3058015354b33d18013bf6cd9b76f80a51d
haakon-soegnen <hknsgn@gmail.com>
2015-03-26 08:01:36 +0100

    Locations in club editor

--- app/club/app.js
+++ app/club/app.js
@@ -2,7 +2,7 @@
 
 	var clubApp = angular.module('clubApp', [ 'ngRoute', 'ui.bootstrap', 'ui.select', 'ngSanitize', 'ngCookies', 
 	                                                'angular-loading-bar',
-	                                                'fnlf-login','imageupload', 'resolve', 'angled-navbar.directives']);
+	                                                'fnlf-login','imageupload', 'resolve', 'angled-navbar.directives','ngMap']);
 
 	clubApp.config([ 'cfpLoadingBarProvider',
 			function(cfpLoadingBarProvider) {
@@ -11,6 +11,59 @@
 			} ]);
 
 })();
+
+angular.module("clubApp").controller("locationsController",
+							['$scope', '$http', 'clubService', '$timeout','$rootScope', '$window',
+							function($scope, $http, clubService, $timeout, $rootScope, $window) {
+
+	// Menus
+	$rootScope.nav = {toolbar: [], menus: []}; //reset
+	$rootScope.nav.brand = 'FNLF Klubb';
+
+	$scope.zoom=7;
+
+	$scope.getClubLocations = function(){
+
+		clubService.getClubLocations().then(function(response){
+    		$scope.loc = response.locations[0];
+    		$scope.clubLocations = response.locations;
+    	});
+
+	};
+
+	$scope.saveClubLocations = function(){
+
+		clubService.getClubLocations().then(function(response){
+
+    		var etag = response.etag;
+
+				clubService.saveClubLocations($scope.clubLocations, etag,response._id).success(function(response) {
+					console.log(response);
+					$scope.clubLocations = response.locations;
+				});
+
+    	});
+
+	};
+
+
+		$scope.dragMarker = function(event,location){
+			location.geo.coordinates[0]=event.latLng.k;
+			location.geo.coordinates[1]=event.latLng.D;
+			console.log(location.geo);
+
+		};
+
+		$scope.clickMarker = function(event,location){
+			$scope.loc=location;
+			$scope.zoom=13;
+			$scope.location = location;
+			$scope.nickname = location.nickname;
+		};
+
+
+
+}]);
 
 angular.module("clubApp").controller("clubController", 
 							['$scope', '$http', 'clubService', '$timeout','$rootScope', '$window',
@@ -25,12 +78,26 @@ angular.module("clubApp").controller("clubController",
 
 
 
+
 angular.module("clubApp")
 	   .service("clubService",['$http','$q','$rootScope', function($http, $q, $rootScope) {
 							
 			var urlBase = '/api/v1';
-	
-	
+
+		   var config = {headers: {
+					'Accept': 'application/json'
+				}
+			};
+
+			this.getClubLocations = function () {
+				return $http.get(urlBase+'/clubs/' + $rootScope.default_club + '?projection={"locations": 1}' ,config).then(handleSuccess, handleError);
+			};
+
+			this.saveClubLocations = function(locations, etag,id) {
+				config.headers['If-Match'] = etag;
+				config.headers['Content-Type'] = 'application/json';
+				return $http({ method: 'PATCH', url: urlBase+'/clubs/' + id, data: {locations: locations}, headers: config.headers});
+			};
 			
 			this.getClubLogo = function() {
 				
