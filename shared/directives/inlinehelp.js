a3081c567f054fb8ddc095a32a02fe8e89fceb8c
Håkon Søgnen <hakon.sognen@bouvet.no>
2015-09-15 13:55:34 +0200

    #34 Nicer inline help

--- shared/directives/inlinehelp.js
+++ shared/directives/inlinehelp.js
@@ -14,7 +14,7 @@ inlinehelp.directive('inlinehelp',  function() {
 
 	directive.template = function(tElement, tAttrs) {
 		
-		return '<span ng-click="flip()"><span><label ng-bind="title"></label> <span class="h3"> <i class="fa fa-question-circle fa-fw text-primary"></i> </span> </span><div collapse="isCollapsed"><div class="panel panel-default"><div class="panel-body" ng-transclude></div></div></div> </span>';
+		return '<span ng-click="flip()"><span><label ng-bind="title"></label> <span class=""> <i class="fa fa-question-circle fa-fw"></i> </span> </span><div collapse="isCollapsed"><div class="panel panel-default"><div class="panel-body inlinehelpbody"><div ng-transclude></div> </div></div></div> </span>';
 
 	};
 
