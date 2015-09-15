e35d7dfdae689ab55dd7f9f01004dec7c157f911
Einar Huseby <einar.huseby@gmail.com>
2015-09-15 18:23:02 +0200

    Fixes #237

--- app/obs/components/observation/directives/observationdateselector.js
+++ app/obs/components/observation/directives/observationdateselector.js
@@ -18,25 +18,24 @@ angular.module('reportingApp').directive('observationdateselector', function ($t
 		if(typeof $scope.observation.when == 'undefined') {
 			$scope.observation.when = new Date();
 		}
-      $scope.open = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
+		$scope.open = function($event) {
+			$event.preventDefault();
+			$event.stopPropagation();
+			$scope.opened = true;
+		};
 
-        $scope.opened = true;
-      };
-
-      $scope.dateOptions = {
-    		    startingDay: 1,
-    		    showWeeks: true
-      };
-      $scope.formats = ['yyyy-MM-dd HH:mm', 'yyyy-MM-ddTHH:mm:ss', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-      $scope.format = $scope.formats[0];
-
-      $scope.maxDate = new Date();
-      $scope.minDate = new Date(2011,10,30);
-      /** TIME **/
-      $scope.hstep = 1;
-      $scope.mstep = 1;
+		$scope.dateOptions = {
+				startingDay: 1,
+				showWeeks: true
+		};
+		
+		$scope.format = 'yyyy-MM-dd HH:mm';
+		
+		$scope.maxDate = new Date();
+		$scope.minDate = new Date(2011,10,30);
+		/** TIME **/
+		$scope.hstep = 1;
+		$scope.mstep = 1;
 
 	};
 
