e983770b3dfdbd6437c6ca1e8b389a2177e9c58f
Håkon Søgnen <hakon.sognen@bouvet.no>
2015-09-15 16:05:20 +0200

    #230 Added another delete button to component editor

--- shared/directives/inlinehelp.js
+++ shared/directives/inlinehelp.js
@@ -14,7 +14,7 @@ inlinehelp.directive('inlinehelp',  function() {
 
 	directive.template = function(tElement, tAttrs) {
 		
-		return '<span ng-click="flip()"><span><label ng-bind="title"></label> <span class=""> <i class="fa fa-question-circle fa-fw"></i> </span> </span><div collapse="isCollapsed"><div class="panel panel-default"><div class="panel-body inlinehelpbody"><div ng-transclude></div> </div></div></div> </span>';
+		return '<span ng-click="flip()"><span><label ng-bind="title"></label> <span class=""> <i class="fa fa-question-circle fa-fw"></i> </span> </span><div collapse="isCollapsed"><div class=""><div class="inlinehelpbody"><div ng-transclude></div> </div></div></div> </span>';
 
 	};
 
