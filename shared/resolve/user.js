24b83808bfaf91515503e049257a3484bb20e897
Håkon Søgnen <hakon.sognen@bouvet.no>
2015-09-22 15:25:39 +0200

    Added watch to userid. Somtimes not set on link

--- shared/resolve/user.js
+++ shared/resolve/user.js
@@ -1,34 +1,40 @@
 angular.module('resolve')
-        .directive('resolveuser', function ($http, $q, ResolveService) {
+	.directive('resolveuser', function ($http, $q, ResolveService) {
 
-                var directive = {};
+		var directive = {};
 
-                directive.restrict = 'E';
-                directive.template = '<span>{{::firstname}} {{::lastname}}</span>';
+		directive.restrict = 'E';
+		directive.template = '<span>{{::firstname}} {{::lastname}}</span>';
 
-                directive.scope = {
-                        userid: '=',
-                        tmpname: '='
-                };
+		directive.scope = {
+			userid: '=',
+			tmpname: '='
+		};
 
-                directive.link = function ($scope, element, attrs) {
-                        if ($scope.userid > 0) {
-                                ResolveService.getUser($scope.userid).then(
-                                        function (user) {
-                                                $scope.firstname = user.firstname;
-                                                $scope.lastname = user.lastname;
-                                        });
-                        }
-                        else{
-                                $scope.firstname = 'Anonymisert';
-                                $scope.lastname = '';
-                                if($scope.tmpname){
-                                        $scope.firstname = $scope.tmpname;
-                        $scope.lastname = '';
-                                }
+		directive.link = function ($scope, element, attrs) {
+			var unbind = $scope.$watch('userid', function () {
+				if ($scope.userid) {
+					if ($scope.userid > 0) {
+						ResolveService.getUser($scope.userid).then(
+							function (user) {
+								$scope.firstname = user.firstname;
+								$scope.lastname = user.lastname;
+							});
+					}
+					else {
+						$scope.firstname = 'Anonymisert';
+						$scope.lastname = '';
+						if ($scope.tmpname) {
+							$scope.firstname = $scope.tmpname;
+							$scope.lastname = '';
+						}
 
-                        }
+					}
+					unbind();
+				}
+			});
 
-                };
-                return directive;
-        });
+
+		};
+		return directive;
+	});
