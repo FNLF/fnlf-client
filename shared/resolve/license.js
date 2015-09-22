31dae96b06c07c205d108afba4e83fa2dc981cb8
Håkon Søgnen <hakon.sognen@bouvet.no>
2015-09-22 15:25:56 +0200

    Added watch to licenseid. Somtimes not set on link

--- shared/resolve/license.js
+++ shared/resolve/license.js
@@ -1,25 +1,32 @@
-
 angular.module('resolve').directive('resolvelicense', function (ResolveService) {
-		
+
 	var directive = {};
-	
+
 	directive.restrict = 'E';
 //	directive.template = '<a href="/app/license/{{licenseid}}">{{name}}</a>';
-	directive.template = '{{::name}}';
-	
+	directive.template = '{{name}}';
+
 	directive.scope = {
-			
-			licenseid: '=',
+
+		licenseid: '=',
 	};
-	
+
 	directive.link = function ($scope, element, attrs) {
-	
-		ResolveService.getLicense($scope.licenseid).then(function(license) {
-			
-			$scope.name = license.name;
+
+		var unbind = $scope.$watch('licenseid', function () {
+			if ($scope.licenseid) {
+				$scope.name = $scope.licenseid;
+				ResolveService.getLicense($scope.licenseid).then(function (license) {
+					$scope.name = license.name;
+				});
+				unbind();
+			}
+
 		});
+
+
 	};
-	
+
 	return directive;
-			
+
 });
