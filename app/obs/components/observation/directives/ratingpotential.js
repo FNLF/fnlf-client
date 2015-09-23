7d459d23b51c68315fb6853e16b68b1fb2e4e65e
Einar Huseby <einar.huseby@gmail.com>
2015-09-23 16:20:30 +0200

    Fixes #261

--- app/obs/components/observation/directives/ratingpotential.js
+++ app/obs/components/observation/directives/ratingpotential.js
@@ -6,9 +6,9 @@ angular.module('reportingApp').directive('ratingPotential', function () {
 		//directive.templateUrl = "components/observation/directives/organization.html";
 		directive.template = function(tElement, tAttrs) { 
 			
-			return '{{ratingType}}: <rating ng-model="observation.rating.potential" max="10" readonly="false" state-on="\'fa fa-bullseye\'" state-off="\'fa fa-circle-o\'" on-hover="hoveringOver(value)" on-leave="overStar = null"> \
+			return '{{ratingType}}: <rating ng-model="observation.rating.potential" max="8" readonly="false" state-on="\'fa fa-bullseye\'" state-off="\'fa fa-circle-o\'" on-hover="hoveringOver(value)" on-leave="overStar = null"> \
 					</rating> \
-			<span class="label" ng-class="{\'label-success\': overStar<3, \'label-info\': overStar>=3 && overStar<5,\'label-warning\': overStar>=5 && overStar<7, \'label-danger\': overStar>=7}" \
+					<span class="label" ng-class="{\'label-success\': overStar<3, \'label-info\': overStar>=3 && overStar<6,\'label-warning\': overStar>=5 && overStar<7, \'label-danger\': overStar>=7}" \
 					ng-show="overStar && !isReadonly">{{overStar}}</span>';
 		};
 
