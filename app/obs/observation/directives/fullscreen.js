6ad863c805c16695b388f67a2747cef7db683dbc
Håkon Søgnen <hknsgn@gmail.com>
2016-07-18 13:58:05 +0200

    Scroll to top when entering fullscreen, scroll to id when exiting

--- app/obs/observation/directives/fullscreen.js
+++ app/obs/observation/directives/fullscreen.js
@@ -1,4 +1,4 @@
-angular.module('reportingApp').directive('fullscreen', function ($location) {
+angular.module('reportingApp').directive('fullscreen', function ($location,$window,$timeout,$document) {
 	var directive = {};
 
 	directive.restrict = 'E';
@@ -8,7 +8,8 @@ angular.module('reportingApp').directive('fullscreen', function ($location) {
 	directive.scope = {
 		observation: '=',
 		acl: '=',
-		title: '@'
+		title: '@',
+		fullscreen: '='
 	};
 
 	directive.link = function ($scope, element, attrs) {
@@ -16,6 +17,23 @@ angular.module('reportingApp').directive('fullscreen', function ($location) {
 		$scope.hideFullscreen = function(){
 			$location.search('ui','');
 		};
+
+		$window.scrollTo(0, 0);
+
+		$scope.$on("$destroy", function() {
+			var fullscreen = $scope.fullscreen;
+			$timeout(function(){
+				var elem = $document[0].getElementById(fullscreen);
+					if(elem){
+						$window.scrollTo(0, elem.offsetTop-70);
+					}else{
+						console.log("Not scrolling to "+fullscreen);
+					}
+
+
+			},0);
+
+		});
 
 	};
 
