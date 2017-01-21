aed084bdd1ae7e9008b8e58d1f3be75b7cdc50bf
haakon-soegnen <hknsgn@gmail.com>
2017-01-21 10:01:02 +0100

    Try to use history.back to close fullscreen

--- app/obs/observation/directives/fullscreen.js
+++ app/obs/observation/directives/fullscreen.js
@@ -1,54 +1,58 @@
-angular.module('reportingApp').directive('fullscreen', function ($location,$window,$timeout,$document,$rootScope) {
-	var directive = {};
+	angular.module('reportingApp').directive('fullscreen', function ($location,$window,$timeout,$document,$rootScope) {
+		var directive = {};
 
-	directive.restrict = 'E';
-	directive.transclude=true;
-	directive.templateUrl = "observation/directives/fullscreen.html";
+		directive.restrict = 'E';
+		directive.transclude=true;
+		directive.templateUrl = "observation/directives/fullscreen.html";
 
-	directive.scope = {
-		observation: '=',
-		acl: '=',
-		fullscreen: '=',
-		titleText: '@',
-		observationChanges:'='
-	};
-
-	directive.link = function ($scope, element, attrs) {
-
-		$scope.isFinished=false;
-
-		$scope.hideFullscreen = function(){
-			$scope.isFinished=true;
-			$timeout(function(){
-				if($scope.isFinished){
-					if ($location.$$search.ui) {
-						$location.search('ui','').replace();
-					}
-				}
-			},0);
+		directive.scope = {
+			observation: '=',
+			acl: '=',
+			fullscreen: '=',
+			titleText: '@',
+			observationChanges:'='
 		};
 
+		directive.link = function ($scope, element, attrs) {
+
+			$scope.isFinished=false;
+
+			$scope.hideFullscreen = function(){
+				if(history.length > 2){
+					history.back();
+				}else{
+					$scope.isFinished=true;
+					$timeout(function(){
+						if($scope.isFinished){
+							if ($location.$$search.ui) {
+								$location.search('ui','').replace();
+							}
+						}
+					},0);
+				}
+			};
 
 
 
 
-		$window.scrollTo(0, 0);
-		var fullscreen = $scope.fullscreen;
-		$scope.$on("$destroy", function() {
-			$timeout(function(){
-				var elem = $document[0].getElementById(fullscreen);
-					if(elem){
-						$window.scrollTo(0, elem.offsetTop-70);
-					}else{
-						console.log("Not scrolling to "+fullscreen);
-					}
+
+			$window.scrollTo(0, 0);
+			var fullscreen = $scope.fullscreen;
+			$scope.$on("$destroy", function() {
+				$timeout(function(){
+					var elem = $document[0].getElementById(fullscreen);
+						if(elem){
+							$window.scrollTo(0, elem.offsetTop-70);
+						}else{
+							console.log("Not scrolling to "+fullscreen);
+						}
 
 
-			},0);
+				},0);
 
-		});
+			});
 
-	};
+		};
 
-	return directive;
-});
+		return directive;
+	});
