7a6ac8ed2e46ccdb49c743c637d982ed73971df5
haakon-soegnen <hknsgn@gmail.com>
2017-06-04 21:09:00 +0200

    Use limit in new ui-select version to make singlepersonselector

--- shared/directives/singlepersonselector.js
+++ shared/directives/singlepersonselector.js
@@ -13,67 +13,48 @@
 		directive.scope = {
 			personmodel:'=',
 			noun: '@',
-			selectfn: '=',
 			acl: '='
 		};
 
 		directive.link = function ($scope, element, attrs, ctrl) {
 
-			$scope.selectedName = 'X';
+			$scope.model=[];
 
-			if($scope.personmodel){
-					ResolveService.getUserName($scope.personmodel.id)
-						.then(function(username){
-							console.log('watch:  name '+username);
-							$scope.personmodel.fullname = username;
-							//$scope.selectedName=username;
-						});
-           		}
+			var unset = $scope.$watch('personmodel',function(){
+				if($scope.personmodel){
+					$scope.model.push($scope.personmodel);
+				}
 
-			$scope.$watch('personmodel',function(){
-           		if($scope.personmodel){
-					ResolveService.getUserName($scope.personmodel.id)
-						.then(function(username){
-							console.log('watch:  name '+username);
-							$scope.personmodel.fullname = username;
-							$scope.selectedName=username;
-						});
-           		}
+				unset();
             });
 
-			$scope.onSelect = function(item, model,label){
 
-				console.log("onselect: "+item+" "+label);
+			$scope.onSelect = function (item, model) {
 
-				console.log(model);
-				//console.log($scope.selectedName);
-				$scope.personmodel=model;
+				$scope.personmodel=item;
+			};
 
-				if($scope.selectfn){
-					$scope.selectfn($scope.personmodel);
+			$scope.onRemove = function(item,model){
+				var index = $scope.model.indexOf(model);
+
+				if (index > -1) {
+					$scope.model.splice(index,1);
 				}
-			};
-
-			$scope.onBlur = function(item,model){
-
-				//$scope.onSelect(item,$scope.model);
+				$scope.personmodel=null;
 			};
 
 
-			$scope.formatLabel = function(model){
-				console.log('format ');
-				console.log(model);
-				return model.fullname;
-				//return $scope.selectedName
-			};
-
-
-
+			$scope.personsFromDb = [];
 
 			$scope.getPersonsByName = function (name) {
-				return RestService.getUserByName(name)
+				RestService.getUserByName(name)
 					.then(function (response) {
-						return response._items;
+						var persons = {};
+						$scope.model.forEach(function(m){
+							persons[m.id]=true;
+						});
+
+						$scope.personsFromDb = response._items.filter(function(p){return !persons[p.id]});
 					});
 			};
 
@@ -87,6 +68,7 @@
 
 
 		};
+
 
 		return directive;
 	};
