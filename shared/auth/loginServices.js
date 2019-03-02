f839c9a94d563ec626015a2215b114e304c720fc
Einar Huseby <einar.huseby@gmail.com>
2019-03-02 17:40:02 +0100

    JSON stringify on object

--- shared/auth/loginServices.js
+++ shared/auth/loginServices.js
@@ -62,7 +62,7 @@
 				return $location.absUrl();
 			}
 			this.getAccessToken = function () {
-				$window.alert('Olbjørn, hva ser du: ' + $location.search());
+				$window.alert('Olbjørn, hva ser du: ' + JSON.stringify($location.search()));
 				if ($location.url().indexOf('access_token') > -1) {
 					
 					return $location.search()['access_token']; // ['access_token'];
